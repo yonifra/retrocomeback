e0110a94926f6ebac189ee21090ea8a3e27331c3
Yoni Fraimorice <yonifra@gmail.com>
2026-02-16 15:22:12 +0200

    feat: update next.config.ts to enhance image handling and security policies

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
         hostname: "placehold.co",
       },
     ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
